9ee765c05e5d8cbc4479ab3ac37cf291612aceaf
anonymoussc <50c5ac69@opayq.com>
2018-06-28 10:21:37 +0700

    Modified article.js src\templates

--- src/templates/article.js
+++ src/templates/article.js
@@ -1,24 +1,17 @@
-import React from 'react'
+import React from 'react'  
 import Link from 'gatsby-link'
 
-const ArticleTemplate = ({ data }) => (
+const ArticleTemplate = ({ data }) => (  
   <div>
     <h1>{data.strapiArticle.title}</h1>
-    <p>
-      by{' '}
-      <Link to={`/authors/${data.strapiArticle.author.id}`}>
-        {data.strapiArticle.author.username}
-      </Link>
-    </p>
+    <p>by <Link to={`/authors/${data.strapiArticle.author.id}`}>{data.strapiArticle.author.username}</Link></p>
     <p>{data.strapiArticle.content}</p>
   </div>
 )
-
 export default ArticleTemplate
-
-export const query = graphql`
+export const query = graphql`  
   query ArticleTemplate($id: String!) {
-    strapiArticle(id: { eq: $id }) {
+    strapiArticle(id: {eq: $id}) {
       title
       content
       author {
