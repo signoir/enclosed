0b3f4e8fcb087b562c8deef2f9267810060f8142
anonymoussc <50c5ac69@opayq.com>
2018-06-28 10:06:01 +0700

    Modified gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,25 +1,21 @@
-const path = require(`path`);
-
+const path        = require(`path`);
 const makeRequest = (graphql, request) => new Promise((resolve, reject) => {
   // Query for article nodes to use in creating pages.
   resolve(
     graphql(request).then(result => {
       if (result.errors) {
-        reject(result.errors)
+        reject(result.errors);
       }
-
       return result;
     })
-  )
+  );
 });
-
 
 // Implement the Gatsby API “createPages”. This is called once the
 // data layer is bootstrapped to let plugins create pages from data.
-exports.createPages = ({ boundActionCreators, graphql }) => {
-  const { createPage } = boundActionCreators;
-
-  const getArticles = makeRequest(graphql, `
+exports.createPages = ({boundActionCreators, graphql}) => {
+  const {createPage} = boundActionCreators;
+  const getArticles  = makeRequest(graphql, `
     {
       allStrapiArticle {
         edges {
@@ -31,18 +27,17 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
     }
     `).then(result => {
     // Create pages for each article.
-    result.data.allStrapiArticle.edges.forEach(({ node }) => {
+    result.data.allStrapiArticle.edges.forEach(({node}) => {
       createPage({
-        path: `/${node.id}`,
+        path     : `/${node.id}`,
         component: path.resolve(`src/templates/article.js`),
-        context: {
+        context  : {
           id: node.id,
         },
-      })
-    })
+      });
+    });
   });
-
-  const getAuthors = makeRequest(graphql, `
+  const getAuthors   = makeRequest(graphql, `
     {
       allStrapiUser {
         edges {
@@ -54,20 +49,19 @@ exports.createPages = ({ boundActionCreators, graphql }) => {
     }
     `).then(result => {
     // Create pages for each user.
-    result.data.allStrapiUser.edges.forEach(({ node }) => {
+    result.data.allStrapiUser.edges.forEach(({node}) => {
       createPage({
-        path: `/authors/${node.id}`,
+        path     : `/authors/${node.id}`,
         component: path.resolve(`src/templates/user.js`),
-        context: {
+        context  : {
           id: node.id,
         },
-      })
-    })
+      });
+    });
   });
-
-  // Query for recipe nodes to use in creating pages.
+  // Queries for articles and authors nodes to use in creating pages.
   return Promise.all([
     getArticles,
     getAuthors,
-  ])
+  ]);
 };
