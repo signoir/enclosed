d098188fb3bd5aae97667c34c97748f0f39398bb
anonymoussc <50c5ac69@opayq.com>
2018-06-28 10:04:07 +0700

    Modified gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,17 +2,17 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  plugins: [
+  plugins     : [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: `http://localhost:1337`,
-        contentTypes: [ // List of the Content Types you want to be able to request from Gatsby.
+        apiURL      : `http://localhost:5000`,
+        contentTypes: [
           `article`,
           `user`
         ]
       },
     },
   ],
-}
+};
